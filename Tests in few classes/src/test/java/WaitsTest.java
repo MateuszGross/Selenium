5db3a4ed5dab6d494696d9340bed0cf8efb07c23
MateuszGross <mateuszgross1@gmail.com>
2020-01-16 16:23:50 +0100

    Waiting for element. Custom method with lambda.

--- Tests in few classes/src/test/java/WaitsTest.java
+++ Tests in few classes/src/test/java/WaitsTest.java
@@ -1,11 +1,8 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.function.Function;
 
 
 public class WaitsTest extends BaseSeleniumTest {
@@ -23,20 +20,16 @@ public class WaitsTest extends BaseSeleniumTest {
 
     private void waitForWebElement(By locator) {
         WebDriverWait wait = new WebDriverWait(driver, 10L);
-
-        wait.until(new Function<WebDriver, Boolean>() {
-            @Override
-            public Boolean apply(WebDriver driver) {
-                System.out.println("Sprawdzam czy element jest wyświetlony");   //LUB   List<WebElement> elements = driver.findElements(locator); przy uzyciu listy wchodzimy do elsa, w przypadku bez listy pomijamy else ponieważ ignorujemy wyjątek
-                if(driver.findElement(locator).isDisplayed()) {                         //if(elements.size()>0) {
-                    System.out.println("Element jest wyświetlony");
-                    return true;
-                } else {
-                    System.out.println("Element nie jest wyświetlony");
-                    return false;
-                    }
-                }
-            });
-        }
+        wait.until(driver1 -> {
+            System.out.println("Sprawdzam czy element jest wyświetlony");   //LUB   List<WebElement> elements = driver.findElements(locator); przy uzyciu listy wchodzimy do elsa, w przypadku bez listy pomijamy else ponieważ ignorujemy wyjątek
+            if (driver.findElement(locator).isDisplayed()) {                         //if(elements.size()>0) {
+                System.out.println("Element jest wyświetlony");
+                return true;
+            } else {
+                System.out.println("Element nie jest wyświetlony");
+                return false;
+            }
+        });
     }
+}
 
