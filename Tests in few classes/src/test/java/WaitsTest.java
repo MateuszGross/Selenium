a7e66151891d9740e732302de1fa0f7be2b94767
MateuszGross <mateuszgross1@gmail.com>
2020-01-16 14:44:16 +0100

    Creating waitForWebElement method which is checking that our paragraph appear on website + using pollingEvery to refresh querying about our element ever 0.5sec. The pollingEvery method is using to increase speed of our tests.

--- Tests in few classes/src/test/java/WaitsTest.java
+++ Tests in few classes/src/test/java/WaitsTest.java
@@ -1,19 +1,31 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+
 
 public class WaitsTest extends BaseSeleniumTest {
 
     @Test
     public void waitTest() {
-        driver.manage().timeouts().implicitlyWait(5L, TimeUnit.SECONDS);
+
         driver.get("C:/Users/Admin/Desktop/JAVA/Selenium/Waits2.html");
         driver.findElement(By.id("clickOnMe")).click();
+        waitForWebElement(By.tagName("p"));
         WebElement paragraph = driver.findElement(By.tagName("p"));
         System.out.println(paragraph.getText());
         Assert.assertTrue(paragraph.getText().equals("Dopiero się pojawiłem!"));
+    }
+
+    private void waitForWebElement(By locator) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10L);
+        ((WebDriverWait) wait).pollingEvery(Duration.ofMillis(500)); //wykonuje zapytanie o element którego poszukujemy
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
