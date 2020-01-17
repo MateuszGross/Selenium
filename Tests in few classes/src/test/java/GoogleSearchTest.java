3c99266656c7e3dcb5dc9cc060249d5ee4c7237e
MateuszGross <mateuszgross1@gmail.com>
2020-01-17 12:43:34 +0100

    @FindBy and PageFactory

--- Tests in few classes/src/test/java/GoogleSearchTest.java
+++ Tests in few classes/src/test/java/GoogleSearchTest.java
@@ -1,16 +1,21 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 
 public class GoogleSearchTest extends BaseSeleniumTest{
 
+    @FindBy(name = "q")
+    private  WebElement searchInput;
+
     @Test
     public void googleSearchTest(){
         SeleniumHelper helper = new SeleniumHelper(driver);
+        PageFactory.initElements(driver, this ); //lub (driver, GoogleSearchTest.class); page factory inicjaluzuje nam search input
         driver.get("https://www.google.com");
-        WebElement searchInput = driver.findElement(By.name("q"));
         searchInput.sendKeys("Selenium");
         helper.takeScreenshot();
         searchInput.sendKeys(Keys.ENTER);
@@ -18,4 +23,10 @@ public class GoogleSearchTest extends BaseSeleniumTest{
         seleniumPageLink.click();
         helper.takeScreenshot();
     }
+
+    /*@Test
+    public void googleSearchTest2(){ //przy użyciu page factory możemy odwoływać się do searchInput z każdego testu w klasie
+        searchInput
+    }*/
+
 }
